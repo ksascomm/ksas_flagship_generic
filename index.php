43d6782b4dc28df7d5bf7562e66a3d32fef3ac56
ksascomm <ksasweb@jhu.edu>
2017-07-28 12:53:53 -0400

    rollback to initial index.php

--- index.php
+++ index.php
@@ -4,35 +4,26 @@
 <?php locate_template('parts-nav-sidebar.php', true, false); ?>	
 	<div class="nine columns wrapper radius-left offset-topgutter">
 		<?php $theme_option = flagship_sub_get_global_options(); ?>	
-		<main class="content post-archive">
-		<h1 class="page-title"><?php echo $theme_option['flagship_sub_feed_name']; ?> Archive</h1>
-		
-		<?php $paged = (get_query_var('paged')) ? (int) get_query_var('paged') : 1;
-				$news_archive_query = new WP_Query(array(
-					'post_type' => 'post',
-					'posts_per_page' => 10,
-					'paged' => $paged
-				)); 
+		<section class="content">
+		<h2><?php echo $theme_option['flagship_sub_feed_name']; ?></h2>
 
-		while ($news_archive_query->have_posts()) : $news_archive_query->the_post(); ?>
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<div class="row">		
-		<article id="post-<?php the_ID(); ?>" itemscope="itemscope" itemtype="http://schema.org/BlogPosting" itemprop="blogPost">
-			<h5 class="black" itemprop="datePublished"><?php the_date(); ?></h5>
-			<h3><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h3>
-			<?php if ( has_post_thumbnail()) { ?> 
-				<?php the_post_thumbnail('thumbnail', array('class'	=> "floatleft")); ?>
-			<?php } ?>
-			<?php the_excerpt(); ?>
+		<section class="twelve columns">
+				<a href="<?php the_permalink(); ?>">
+					<h6><?php the_date(); ?></h6>
+					<h5><?php the_title();?></h5>
+					<?php if ( has_post_thumbnail()) { ?> 
+						<div class="floatleft">
+							<?php the_post_thumbnail('thumbnail'); ?>
+						</div>
+					<?php } ?>
+					<?php the_excerpt(); ?>
+				</a>
 				<hr>
-		</article>
-		</div>
-	
-	<?php endwhile;?>
-		<div class="row">
-			<?php flagship_pagination($news_archive_query->max_num_pages); ?>		
-		</div>
-
-	</main>	
+		</section>
+	</div>
+	<?php endwhile; endif; ?>		
 	</div>	<!-- End main content (left) section -->
 </div> 
 <?php get_footer(); ?> 
